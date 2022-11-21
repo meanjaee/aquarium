eae30cd857a94584216234f7724a1d3fb3ba9afd
MJPub <minsigner@gmail.com>
2022-11-21 02:24:34 +0000

    mainpc

--- js/lotteaquarium_menu.js
+++ js/lotteaquarium_menu.js
@@ -18,26 +18,4 @@ $(document).ready(function () {
             return text === 'CLOSE' ? 'MENU' : 'CLOSE';
         });1
     });
-
-    $("footer .right select").click(function () {
-                $("select").css("border", "none");
-            });
-
-            $(".tab").click(function () {
-                $(".tab .content").toggleClass("on");
-                $(".img").css("display:none");
-                $(this).css("display:none");
-            });
-        
-            $("footer .left button").click(function () {
-                $(".foot").toggleClass("active");
-            });
-            
-            if($(window).width()<640){
-                $(".global_02 h2").click(function(){
-                $(this).next("ul").stop().slideToggle()
-            })
-            }
-
-           
 });
